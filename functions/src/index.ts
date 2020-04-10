2162092bf3be0f2986e865b79d158e184919b8d6
Yuuki Wesp <ls-micro@ya.ru>
2020-04-10 19:04:18 +0300

    rm firestore.ts, update typescript, move init to middleware

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
-import { getDB } from "./Firestore";
+import { initializeApp, firestore } from "firebase-admin";
+import { config } from "firebase-functions";
 import { getInstallationToken } from "./github";
 import on from "./on";
 import express from "express";
+initializeApp(config().firebase);
+process.env["DEBUG"] = "express:*";
 const app = express();
+const db = firestore();
+
+
+app.use(async(req, res, next) => {
+    if(!process.env["APP_ID"])
+    {
+        const c = await db.collection("cluster").doc("config").get();
+        process.env["APP_ID"] = c.get("AppID");
+        process.env["PRIVATE_KEY"] = Buffer.from(c.get("Certificate"), 'base64').toString("utf8");
+        process.env["INST_ID"] = c.get("InstallationID");
+    }
+    next();
+});
 
 app.post('/@me/hook', (req, res) => {
     console.log(req.body);
@@ -11,29 +27,23 @@ app.post('/@me/hook', (req, res) => {
 app.get('/@me/container/github', async (req, res) => {
     const authKey = req.header("Authorization");
     if(!authKey)
-        return res.sendStatus(401);
-    const credentials = await getDB().collection("cluster").doc("credentials").get();
+    {
+        res.sendStatus(401);
+        return;
+    }
+    const credentials = await db.collection("cluster").doc("credentials").get();
     if(credentials.get("api") !== authKey)
+    {
         return res.sendStatus(401);
+        return;
+    }
 
     // @ts-ignore
     const token = await getInstallationToken(+process.env["INST_ID"]);
-    return res.send(token);
+    res.send(token);
+    return;
 });
 
 const main = express();
-
-(async() => {
-    const config = await getDB().collection("cluster").doc("config").get();
-    
-    process.env["APP_ID"] = config.get("AppID");
-    process.env["PRIVATE_KEY"] = Buffer.from(config.get("Certificate"), 'base64').toString("utf8");
-    process.env["INST_ID"] = config.get("InstallationID");
-    main.use('/api', app);
-})();
-
-
-
-
-
+main.use('/api', app);
 exports.main = on(main);
